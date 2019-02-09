48a1048263bee762e482f39ebf697ba893fec624
Brian Lachniet <blachniet@gmail.com>
2019-02-08 22:30:49 -0500

    Add platform_linux.go and multi platform travis configuration

---
+++ platform_linux.go
@@ -0,0 +1,17 @@
+// +build !windows
+
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path"
+)
+
+func getFallbackCacheDirname() string {
+	return path.Join(os.Getenv("HOME"), ".cache")
+}
+
+func openURL(url string) error {
+	return exec.Command("xdg-open", url).Start()
+}
